ffdc571281d4efb51a742f2d210578cf958d62c5
larmork <larmorkk@mail.ru>
2023-02-09 21:37:52 +0300

    time control

--- src/agent.py
+++ src/agent.py
@@ -1,16 +1,15 @@
-from forta_agent import Finding, FindingType, FindingSeverity
+from forta_agent import Finding, FindingType, FindingSeverity, get_transaction_receipt
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-TRANSFER_EVENT = '{"name":"Transfer","type":"event","anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}]}'
-HIGH_APPROVAL_PER_HOUR = 20
-MEDIUM_APPROVAL_PER_HOUR = 10
+HIGH_APPROVAL_PER_HOUR = 25
+MEDIUM_APPROVAL_PER_HOUR = 15
 
 
 findings_count = 0
-n_approval = 0
-
 
 def handle_transaction(transaction_event):
     findings = []
+    dict_addresses = {}
 
     global findings_count
     if findings_count >= 5:
@@ -18,31 +17,41 @@ def handle_transaction(transaction_event):
 
 
     receipt = get_transaction_receipt(transaction_event.hash)
-    if receipt.name = "Approval":
-        n_approval += 1
+    if receipt.logs.topics == "Approval":
+        dict_addresses["{receipt.logs.address}"] += 1
 
-    if n_approval >= MAX_APPROVAL_PER_HOUR:
+    for a in dict_addresses.values():
+        if a >= 10:
+            address = dict_addresses[a]
+            n_approval = a
 
 
-        findings.append(Finding({
-            'name': 'Suspicious amount of token approvals',
-            'description': f'Approved tokens: {n_appproval}',
-            'alert_id': 'FORTA-1',
-            'type': FindingType.Suspicious,
-            'severity': get_severity(n_approval),
-            'metadata': {
-              
-            }
-        }))
-        findings_count += len(findings)
-        return findings
+            findings.append(Finding({
+                'name': 'Suspicious amount of token approvals',
+                'description': f'Approved tokens: {n_approval}',
+                'alert_id': 'FORTA-1',
+                'type': FindingType.Suspicious,
+                'severity': get_severity(n_approval),
+                'metadata': {
+                    'address': address
+                }
+            }))
+
+            def clear_dict():
+                dict_addresses.clear()
+            scheduler = BlockingScheduler()
+            scheduler.add_job(clear_dict, 'interval', hours=1)
+            scheduler.start()
+
+            findings_count += len(findings)
+            return findings
     
     def get_severity(n_approval):
-    if n_approval > HIGH_APPROVAL_PER_HOUR:
-        return FindingSeverity.High
-    elif n_approval > MEDIUM_APPROVAL_PER_HOUR:
-        return FindingSeverity.Medium
-    else:
-        return FindingSeverity.Low
+        if n_approval > HIGH_APPROVAL_PER_HOUR:
+            return FindingSeverity.High
+        elif n_approval > MEDIUM_APPROVAL_PER_HOUR:
+            return FindingSeverity.Medium
+        else:
+            return FindingSeverity.Low
 
 
