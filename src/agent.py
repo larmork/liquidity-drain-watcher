07e713ed99ca4a883f0643524570835812bc6bbe
larmork <larmorkk@mail.ru>
2023-02-10 22:05:02 +0300

    enhancement

--- src/agent.py
+++ src/agent.py
@@ -7,45 +7,45 @@ MEDIUM_APPROVAL_PER_HOUR = 15
 
 findings_count = 0
 
-def handle_transaction(transaction_event):
-    findings = []
-    dict_addresses = {}
+def provide_handle_transaction(get_transaction_receipt):
+    def handle_transaction(transaction_event):
+        findings = []
+        dict_addresses = {}
 
-    global findings_count
-    if findings_count >= 5:
-        return findings
-        
-
-
-    receipt = get_transaction_receipt(transaction_event.hash)
-    if receipt.logs.topics == "Approval":
-        dict_addresses["{receipt.logs.address}"] += 1
-
-    for a in dict_addresses.values():
-        if a >= 10:
-            address = dict_addresses[a]
-            n_approval = a
-
-
-            findings.append(Finding({
-                'name': 'Suspicious amount of token approvals',
-                'description': f'Approved tokens: {n_approval}',
-                'alert_id': 'FORTA-1',
-                'type': FindingType.Suspicious,
-                'severity': get_severity(n_approval),
-                'metadata': {
-                    'address': address
-                }
-            }))
-
-            def clear_dict():
-                dict_addresses.clear()
-            scheduler = BlockingScheduler()
-            scheduler.add_job(clear_dict, 'interval', hours=1)
-            scheduler.start()
-
-            findings_count += len(findings)
+        global findings_count
+        if findings_count >= 5:
             return findings
+        
+        receipt = get_transaction_receipt(transaction_event.hash)
+        if receipt.logs.topics == "Approval":
+            dict_addresses["{receipt.logs.address}"] += 1
+
+        for a in dict_addresses.values():
+            if a >= 10:
+                address = dict_addresses[a]
+                n_approval = a
+
+                findings.append(Finding({
+                    'name': 'Suspicious amount of token approvals',
+                    'description': f'Approved tokens: {n_approval}',
+                    'alert_id': 'FORTA-1',
+                    'type': FindingType.Suspicious,
+                    'severity': get_severity(n_approval),
+                    'metadata': {
+                        'address': address
+                    }
+                }))
+
+                def clear_dict():
+                    dict_addresses.clear()
+                scheduler = BlockingScheduler()
+                scheduler.add_job(clear_dict, 'interval', hours=1)
+                scheduler.start()
+
+                findings_count += len(findings)
+                return findings
+
+        return handle_transaction
     
     def get_severity(n_approval):
         if n_approval > HIGH_APPROVAL_PER_HOUR:
@@ -55,4 +55,7 @@ def handle_transaction(transaction_event):
         else:
             return FindingSeverity.Low
 
+real_handle_transaction = provide_handle_transaction(get_transaction_receipt)
 
+def handle_transaction(transaction_event):
+    return real_handle_transaction(transaction_event)
