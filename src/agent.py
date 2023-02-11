080bd59d26ee8b3ce484479a7c0a86672698e263
larmork <larmorkk@mail.ru>
2023-02-11 16:48:08 +0300

    comments

--- src/agent.py
+++ src/agent.py
@@ -15,15 +15,18 @@ def provide_handle_transaction(get_transaction_receipt):
         global findings_count
         if findings_count >= 5:
             return findings
-        
+        #avoid spamming
+
         receipt = get_transaction_receipt(transaction_event.hash)
         if receipt.logs.topics == "Approval":
             dict_addresses["{receipt.logs.address}"] += 1
+        #create dictionary with data about approvals from each address
 
         for a in dict_addresses.values():
             if a >= 10:
                 address = dict_addresses[a]
                 n_approval = a
+        #hooking addresses with suspicious amount of approvals
 
                 findings.append(Finding({
                     'name': 'Suspicious amount of token approvals',
@@ -41,6 +44,7 @@ def provide_handle_transaction(get_transaction_receipt):
                 scheduler = BlockingScheduler()
                 scheduler.add_job(clear_dict, 'interval', hours=1)
                 scheduler.start()
+        #clear dictionary every hour to check actual info
 
                 findings_count += len(findings)
                 return findings
@@ -54,6 +58,7 @@ def provide_handle_transaction(get_transaction_receipt):
             return FindingSeverity.Medium
         else:
             return FindingSeverity.Low
+        #determining the severity for the alert
 
 real_handle_transaction = provide_handle_transaction(get_transaction_receipt)
 
